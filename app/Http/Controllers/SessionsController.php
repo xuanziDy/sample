86d1c01739e3d30e396a8c758438231febf9a8d9
dy <827352340@qq.com>
2018-09-25 07:59:25 +0000

    Finish login and logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
+use Auth;
 
 class SessionsController extends Controller
 {
@@ -11,4 +12,26 @@ class SessionsController extends Controller
     {
         return view('sessions.create');
     }
+
+    public function store(Request $request)
+    {
+       $credentials = $this->validate($request, [
+           'email' => 'required|email|max:255',
+           'password' => 'required'
+       ]);
+	if(Auth::attempt($credentials,$request->has('remember'))){
+		session()->flash('success','欢迎回来');
+		return redirect()->route('users.show',[Auth::user()]);
+	}else{
+		session()->flash('danger','很抱歉,您的邮箱和密码不匹配');
+		return redirect()->back();
+	}
+    }
+    public function destroy()
+    {
+	Auth::logout();
+	session()->flash('success','您已成功退出');
+	return redirect('login');
+    }
+
 }
