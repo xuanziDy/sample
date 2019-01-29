cc1d3090fd46c85645d8bf29316be5424d4a0d26
dy <827352340@qq.com>
2019-01-29 08:55:22 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -25,20 +25,20 @@ class SessionsController extends Controller
            'email' => 'required|email|max:255',
            'password' => 'required'
        ]);
-	if(Auth::attempt($credentials,$request->has('remember'))){
-		session()->flash('success','欢迎回来');
-		return redirect()->intended(route('users.show',[Auth::user()]));
+    	if(Auth::attempt($credentials,$request->has('remember'))){
+    		session()->flash('success','欢迎回来');
+    		return redirect()->intended(route('users.show',[Auth::user()]));
 
-	}else{
-		session()->flash('danger','很抱歉,您的邮箱和密码不匹配');
-		return redirect()->back();
-	}
+    	}else{
+    		session()->flash('danger','很抱歉,您的邮箱和密码不匹配');
+    		return redirect()->back();
+    	}
     }
     public function destroy()
     {
-	Auth::logout();
-	session()->flash('success','您已成功退出');
-	return redirect('login');
+    	Auth::logout();
+    	session()->flash('success','您已成功退出');
+    	return redirect('login');
     }
 
 }
