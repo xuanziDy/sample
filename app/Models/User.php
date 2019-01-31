39eeccd350f9012b35ca5afaa03b2be574656787
dy <827352340@qq.com>
2019-01-31 08:32:01 +0000

    Add account activations

--- app/Models/User.php
+++ app/Models/User.php
@@ -29,9 +29,19 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    //boot 方法会在用户模型类完成初始化之后进行加载，因此我们对事件的监听需要放在该方法中。
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
     public function gravatar($size = 100)
     {
     	$hash = md5(strtolower(trim($this->attributes['email'])));
-	return "http://www.gravatar.com/avatar/$hash?s=$size";
+	    return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 }
